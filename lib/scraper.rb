caff9a731ed1b11f93e6645bbefec01bd44777ed
Benny Hudson Hung <bennyhudsonhung@gmail.com>
2016-12-06 21:28:05 -0500

    Changed scraper.rb and changed scraped website

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,22 +2,23 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
+require_relative './painting.rb'
+
 class Scraper
 
-  def self.scrape_index_page
-    paintings = []
-    doc = Nokogiri::HTML(open(https://www.artsy.net/gene/renaissance))
-    doc.css(".artist-fillwidth-list-artworks li.artist-fillwidth-row").each do |card|
-      painting = {}
-      painting[:painter] = card.css(".artwork-item-caption p.artwork-item-artist").text
-      painting[:title] = card.css(".artwork-item-caption p.artwork-item-title").text
-      painting[:location] = card.css(".artwork-item-caption p.artwork-item-partner").text
-      painting[:url] = card.css(".artwork-item-image-container a img").attribute("src").value
-
-      paintings.push(painting)
-    end
-    paintings
+  def scrape_site
+    Nokogiri::HTML(open("http://www.brushwiz.com/most-famous-paintings/"))
   end
 
+  def scrape_painting
+    self.scrape_site.css("div.col.col-25")
+  end
+
+  def officialize_painting
+    scrape_painting.each do |piece|
+      Painting.newly_scraped(piece)
+    end
+  end
 
 end
+
