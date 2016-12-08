a911d1922051d11bac826fa7af26f4bf4b3f245e
Benny Hudson Hung <bennyhudsonhung@gmail.com>
2016-12-08 10:59:31 -0500

    added more CLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -33,3 +33,54 @@ class Cli
       search
     end
   end
+
+  def search_by_painter
+    input = gets.strip.downcase
+    if painter.list_all_painters.detect{|painter| painter.downcase == input} != nil
+      puts ""
+      puts "#{painter}(#{painter.painter_nationality}) has #{painter.titles.count} paintings that are included in The Most Famous Paintings of All-Time:"
+      puts ""
+      painter.titles.each do |painting|
+        puts "'#{painting.title}', #{painting.time_period} (#{painting.style}) in #{painting.location}"
+      end
+      puts ""
+      puts "Would you like to search again?"
+      play_again
+    else
+      puts "Sorry, this painter's works are not mentioned in this list of The Most Famous Paintings of All-Time. Please enter another painter's name."
+      search_by_painter
+    end
+  end
+
+  def search_by_title
+    input = gets.strip.downcase
+    if painting.list_all_painting_titles.detect{|painting| painting.downcase == input} != nil
+      puts ""
+      puts "'#{painting}' is included in this list of The Most Famous Paintings of All-Time."
+      puts ""
+      puts "painter:     #{painting.painter}(#{painter.painter_nationality})"
+      puts "time period: #{painting.time_period}"
+      puts "style:       #{painting.style}"
+      puts "location:    #{painting.location}"
+      puts ""
+      puts "Would you like to search for another painting?"
+      play_again
+    else
+      puts "Sorry, this painting is not mentioned in this list of The Most Famous Paintings of All-Time. Please enter another the title of another painting."
+      search_by_title
+    end
+  end
+
+  def play_again
+    input = gets.strip.downcase
+    if input == "yes"
+      search
+    elsif input == "no"
+      puts "Thank you for playing. Please come back to play again soon!"
+    else
+      puts "Invalid entry. Please enter yes or no."
+      play_again
+    end
+  end
+
+end
