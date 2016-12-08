ed2a67253df93ca2c3727e9311f46c8d82626e12
Benny Hudson Hung <bennyhudsonhung@gmail.com>
2016-12-08 10:54:04 -0500

    added new class Painter

---
+++ lib/painter.rb
@@ -0,0 +1,29 @@
+class Painter
+  attr_accessor :titles, :name, :painter_nationality
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @painter_nationality = painter_nationality
+    @titles = []
+  end
+
+  def self.all
+    @@all.uniq
+  end
+
+  def list_all_painters
+    scraper.scrape_paintings.each do |card|
+      url = card.css("a.col-item").attribute("href").text
+      painters = url.css("ul.lined.features > li:nth-child(1) > span > a").text
+      @@all << painters
+    end
+    self.all
+  end
+
+  def add_titles(title)
+    @titles << title
+  end
+
+end
