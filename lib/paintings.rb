d63266597a7771b3f9e3a8a8ff614f346904f5f4
Benny Hudson Hung <bennyhudsonhung@gmail.com>
2016-12-08 00:10:55 -0500

    included all painting attributes

--- lib/paintings.rb
+++ lib/paintings.rb
@@ -3,44 +3,43 @@ require 'open-uri'
 require 'pry'
 
 class Painting
-  attr_accessor :painter, :location, :style, :century
+  attr_accessor :title, :url, :painter, :painter_nationality, :location, :style, :time_period
 
   @@all = []
 
-  def initialize
+  def initialize(title, url, painter, painter_nationality, location, style, time_period)
+    @title = title
+    @url = url
     @painter = painter
+    @painter_nationality = painter_nationality
     @location = location
     @style = style
-    @century = century
+    @time_period = time_period
     @@all << self
-  end
-
-  def self.newly_scraped(piece)
-    self.new(piece.css(".col-item-title").text)
   end
 
   def self.all
     @@all
   end
 
-  def original_painter
-    @painter ||= doc.xpath("//div[@class='mobile-480-hide']/p/a").text
+  # identify attributes of each newly scraped painting
+  def self.new_from_scraped_site(card)
+    url = card.css("a.col-item").attribute("href").text
+    self.new(card.css("a > span.col-item-title").text,
+    url,
+    url.css("ul.lined.features > li:nth-child(1) > span > a").text,
+    url.css("ul.lined.features > li:nth-child(3) > span > a").text,
+    url.css("ul.lined.features > li:nth-child(7) > span > a").text,
+    url.css("ul.lined.features > li:nth-child(5) > span > a").text,
+    url.css("ul.lined.features > li:nth-child(4) > span > a").text)
   end
 
-  def painting_location
-    @location ||= doc.xpath("//div[@class='page-section']/ul[1]/li[7]/a").text
-  end
-
-  def painting_style
-    @style ||= doc.xpath("//div[@class='page-section']/ul[1]/li[5]/a").text
-  end
-
-  def time_period
-    @century ||= doc.xpath("//div[@class='page-section']/ul[1]/li[4]/a").text
-  end
-
-  def doc
-    @doc ||= Nokogiri::HTML(open(self.url))
+  # lists all painting titles
+  def list_all_paintings
+    scraper.scrape_painting.each do |card|
+      title = card("a > span.col-item-title").text
+      puts title
+    end
   end
 
 end
