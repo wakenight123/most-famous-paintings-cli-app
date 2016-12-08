fe8c77b5f8fdea7ae19f5b553b8f48c44e1e7ac8
Benny Hudson Hung <bennyhudsonhung@gmail.com>
2016-12-08 00:07:12 -0500

    Update scraper.rb

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,24 +1,19 @@
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
-
-require_relative './painting.rb'
-
 class Scraper
 
   def scrape_site
     Nokogiri::HTML(open("http://www.brushwiz.com/most-famous-paintings/"))
   end
 
+  # collects total data on all paintings
   def scrape_painting
     self.scrape_site.css("div.col.col-25")
   end
 
-  def officialize_painting
-    scrape_painting.each do |piece|
-      Painting.newly_scraped(piece)
+  # enter all painting.new(title, url) into painting.all
+  def make_painting
+    scraper.scrape_painting.each do |card|
+      painting.new_from_scraped_site(card)
     end
   end
 
 end
-
