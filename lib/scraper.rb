88ab79e721b5b0a04ee84c9a05f8d25905438780
Benny Hung <wakenightryu@gmail.com>
2016-12-14 01:36:39 -0500

    added files

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 require 'nokogiri'
 require 'open-uri'
 
@@ -19,3 +20,27 @@ class Scraper
   end
 
 end
+=======
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class Scraper
+
+  def self.scrape_index_page
+    paintings = []
+    doc = Nokogiri::HTML(open(https://www.artsy.net/gene/renaissance))
+    doc.css(".artist-fillwidth-list-artworks li.artist-fillwidth-row").each do |card|
+      painting = {}
+      painting[:painter] = card.css(".artwork-item-caption p.artwork-item-artist").text
+      painting[:title] = card.css(".artwork-item-caption p.artwork-item-title").text
+      painting[:location] = card.css(".artwork-item-caption p.artwork-item-partner").text
+      painting[:url] = card.css(".artwork-item-image-container a img").attribute("src").value
+
+      paintings.push(painting)
+    end
+    paintings
+  end
+
+end
+>>>>>>> Stashed changes
